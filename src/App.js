cfcc8d6e50bec77de0a6f5e268e8b10259b5148f
Sergey Dyakov <s.dyakov9@gmail.com>
2022-05-20 09:19:27 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Main from './Main/Main';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const theme = extendTheme({
+  shadows: {
+    'solid-left':
+      '0px 2px 0px 0px, -1.5px 1.5px 0px 0px, 0px 0px 0px 0px, 0px 0px 0px 0px, -3px 3px 0px 0px',
+    'solid-off': '0px 0px 0px 0px',
+  },
+});
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
